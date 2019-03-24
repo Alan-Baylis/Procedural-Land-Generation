d7ffe182bda8f3f981012f505dd5acc5448dc0d3
iwantmyfile5 <iwantmyfile5@users.noreply.github.com>
2019-03-24 13:28:33 -0400

    Initial commit

---
+++ Assets/Scripts/Data/TerrainData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu()]
+public class TerrainData : UpdateableData {
+
+    public float uniformScale = 2f;
+
+    public bool useFlatShading;
+    public bool useFalloff;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
+    public float minHeight
+    {
+        get
+        {
+            return uniformScale * meshHeightMultiplier * meshHeightCurve.Evaluate(0);
+        }
+    }
+
+    public float maxHeight
+    {
+        get
+        {
+            return uniformScale * meshHeightMultiplier * meshHeightCurve.Evaluate(1);
+        }
+    }
+}
